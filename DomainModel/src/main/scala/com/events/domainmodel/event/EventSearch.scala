1c5077601df8b3b8b2339db96a9f8d930453f46d
pedrompc <mpcpedro@gmail.com>
2015-03-28 21:21:22 +0000

    Added missing files

---
+++ DomainModel/src/main/scala/com/events/domainmodel/event/EventSearch.scala
@@ -0,0 +1,20 @@
+package com.events.domainmodel.event
+
+import com.events.domainmodel.search.Paging
+import com.github.nscala_time.time.Imports._
+
+/**
+ * @author pedro
+ */
+class EventSearch(
+    val name: String,
+    val userLocation: Location, 
+    val radius: Radius, 
+    val startDate: DateTime,
+    val endDate: DateTime,
+    val categories: Traversable[Category],
+    val tags: Traversable[Tag])
+    (val paging: Option[Paging])
+{ 
+  def this(userLocation: Location, radius: Radius) = this("", userLocation, radius, null, null, List(), List())(None)
+}
---
+++ DomainModel/src/main/scala/com/events/domainmodel/search/Paging.scala
@@ -0,0 +1,11 @@
+package com.events.domainmodel.search
+
+/**
+ * @author pedro
+ */
+class Paging(
+  val startPage: Int,
+  val count: Int) 
+{
+  
+}
